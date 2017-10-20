3a9744a231103f346dd3c1b24552a2319f472e03
Milton Ortega <MIGMANMX@users.noreply.github.com>
2017-10-20 18:52:51 -0500

    20/10/2017

--- RHLogica/ctiCalendario.vb
+++ RHLogica/ctiCalendario.vb
@@ -40,6 +40,7 @@ Public Class ctiCalendario
             dsP(3) = rdr("fin").ToString
             dsP(4) = rdr("color").ToString
             dsP(5) = rdr("id_att").ToString
+            dsP(6) = rdr("cierre").ToString
         Else
             ReDim dsP(1) : dsP(1) = "Error: no se encuentra esta jornada."
         End If
@@ -62,7 +63,7 @@ Public Class ctiCalendario
         rdr.Close() : rdr = Nothing : cmd.Dispose() : dbC.Close() : dbC.Dispose()
         Return dsP
     End Function
-    Public Function agregarJornada(ByVal jornada As String, ByVal inicio As String, ByVal fin As String, ByVal color As String, ByRef id_att As Integer) As String()
+    Public Function agregarJornada(ByVal jornada As String, ByVal inicio As String, ByVal fin As String, ByVal color As String, ByVal id_att As Integer, ByVal cierre As Boolean) As String()
         Dim ans() As String
         If jornada <> "" Then
             Dim dbC As New SqlConnection(StarTconnStrRH)
@@ -76,9 +77,9 @@ Public Class ctiCalendario
                 rdr.Close()
             Else
                 rdr.Close()
-                cmd.CommandText = "INSERT INTO Jornada SELECT @jornada,@inicio,@fin,@color,@id_att"
+                cmd.CommandText = "INSERT INTO Jornada SELECT @jornada,@inicio,@fin,@color,@id_att,@cierre"
 
-                'cmd.Parameters.AddWithValue("jornada", jornada)
+                cmd.Parameters.AddWithValue("cierre", cierre)
                 cmd.Parameters.AddWithValue("inicio", inicio)
                 cmd.Parameters.AddWithValue("fin", fin)
                 cmd.Parameters.AddWithValue("color", color)
@@ -99,7 +100,7 @@ Public Class ctiCalendario
         End If
         Return ans
     End Function
-    Public Function actualizarJornada(ByVal idjornada As Integer, ByVal jornada As String, ByVal inicio As String, ByVal fin As String, ByVal color As String, ByVal id_att As Integer) As String
+    Public Function actualizarJornada(ByVal idjornada As Integer, ByVal jornada As String, ByVal inicio As String, ByVal fin As String, ByVal color As String, ByVal id_att As Integer, ByVal cierre As Boolean) As String
         Dim err As String
         If jornada = "" Then
             err = "Error: no se actualizó, es necesario capturar el puesto de empleado."
@@ -116,7 +117,8 @@ Public Class ctiCalendario
                 err = "Error: no se actualizó, ya existe este nombre."
             Else
                 rdr.Close()
-                cmd.CommandText = "UPDATE Jornada SET jornada = @jornada, inicio = @inicio, fin = @fin, color = @color, id_att = @id_att WHERE idjornada = @idjornada"
+                cmd.CommandText = "UPDATE Jornada SET jornada = @jornada, inicio = @inicio, fin = @fin, color = @color, id_att = @id_att, cierre = @cierre WHERE idjornada = @idjornada"
+                cmd.Parameters.AddWithValue("cierre", cierre)
                 cmd.Parameters.AddWithValue("inicio", inicio)
                 cmd.Parameters.AddWithValue("fin", fin)
                 cmd.Parameters.AddWithValue("color", color)
@@ -154,13 +156,13 @@ Public Class ctiCalendario
         dt.Columns.Add(New DataColumn("inicio", System.Type.GetType("System.String")))
         dt.Columns.Add(New DataColumn("fin", System.Type.GetType("System.String")))
         dt.Columns.Add(New DataColumn("color", System.Type.GetType("System.String")))
-        'dt.Columns.Add(New DataColumn("id_att", System.Type.GetType("System.Int32")))
+        dt.Columns.Add(New DataColumn("cierre", System.Type.GetType("System.Boolean")))
 
 
         Dim r As DataRow
         Dim dbC As New SqlConnection(StarTconnStrRH)
         dbC.Open()
-        Dim cmd As New SqlCommand("SELECT idjornada,jornada,inicio,fin,color,id_att FROM Jornada  ORDER BY jornada", dbC)
+        Dim cmd As New SqlCommand("SELECT idjornada,jornada,inicio,fin,color,cierre FROM Jornada  ORDER BY jornada", dbC)
         'cmd.Parameters.AddWithValue("idjornada", idjornada)
         Dim rdr As SqlDataReader = cmd.ExecuteReader
         While rdr.Read
@@ -170,7 +172,7 @@ Public Class ctiCalendario
             r(2) = rdr("inicio").ToString
             r(3) = rdr("fin").ToString
             r(4) = rdr("color").ToString
-            'r(5) = rdr("id_att").ToString
+            r(5) = rdr("cierre").ToString
 
             dt.Rows.Add(r)
         End While
@@ -221,15 +223,17 @@ Public Class ctiCalendario
             dsP(3) = rdr("fecha").ToString
             dsP(4) = rdr("observaciones").ToString
             dsP(5) = rdr("cantidad").ToString
+            dsP(6) = rdr("fecha_ued").ToString
+
         Else
             ReDim dsP(1) : dsP(1) = "Error: no se encuentra."
         End If
         rdr.Close() : rdr = Nothing : cmd.Dispose() : dbC.Close() : dbC.Dispose()
         Return dsP
     End Function
-    Public Function agregarParticulares(ByVal idempleado As Integer, ByVal tipo As Integer, ByVal fecha As String, ByVal observaciones As String, ByRef cantidad As Integer) As String()
+    Public Function agregarParticulares(ByVal idempleado As Integer, ByVal tipo As String, ByVal fecha As String, ByVal observaciones As String, ByRef cantidad As Integer) As String()
         Dim ans() As String
-        If tipo <> 0 Then
+        If tipo <> "" Then
             Dim dbC As New SqlConnection(StarTconnStrRH)
             dbC.Open()
             Dim cmd As New SqlCommand("SELECT idparticulares FROM Particulares WHERE tipo = @tipo AND fecha = @fecha", dbC)
@@ -242,13 +246,15 @@ Public Class ctiCalendario
                 rdr.Close()
             Else
                 rdr.Close()
-                cmd.CommandText = "INSERT INTO Particulares SELECT @idempleado,@tipo,@fecha,@observaciones,@cantidad"
+                ' cmd.CommandText = "INSERT INTO Particulares SELECT @idempleado,@tipo,@fecha,@observaciones,@cantidad"
+                cmd.CommandText = "INSERT INTO Particulares (idempleado,tipo,fecha,observaciones,cantidad) 
+                    values('" & idempleado & "','" & tipo & "','" & fecha & "','" & observaciones & "','" & cantidad & "')"
                 cmd.Parameters.AddWithValue("idempleado", idempleado)
                 cmd.Parameters.AddWithValue("observaciones", observaciones)
                 cmd.Parameters.AddWithValue("cantidad", cantidad)
 
                 cmd.ExecuteNonQuery()
-                cmd.CommandText = "SELECT idparticulares FROM Particulares WHERE tipo = @tipo AND fecha = @fecha"
+                cmd.CommandText = "SELECT idparticulares FROM Particulares WHERE tipo = @tipo AND fecha = @fecha AND idempleado = @idempleado"
                 rdr = cmd.ExecuteReader
                 rdr.Read()
                 ReDim ans(1)
@@ -263,34 +269,28 @@ Public Class ctiCalendario
         End If
         Return ans
     End Function
-    Public Function actualizarParticulares(ByVal idparticulares As Integer, ByVal idempleado As Integer, ByVal tipo As Integer, ByVal fecha As String, ByVal observaciones As String, ByVal cantidad As Integer, ByVal fecha_ued As String) As String
-        'Dim err As String
-        'If jornada = "" Then
-        '    err = "Error: no se actualizó, es necesario capturar el puesto de empleado."
-        'Else
-        '    Dim dbC As New SqlConnection(StarTconnStrRH)
-        '    dbC.Open()
+    Public Function actualizarParticulares(ByVal idparticulares As Integer, ByVal idempleado As Integer, ByVal tipo As String, ByVal fecha As String, ByVal observaciones As String, ByVal cantidad As Integer, ByVal fecha_ued As String) As String
+        Dim err As String
+        If tipo = "" Then
+            err = "Error: no se actualizó, es necesario capturar"
+        Else
+            Dim dbC As New SqlConnection(StarTconnStrRH)
+            dbC.Open()
 
-        '    Dim cmd As New SqlCommand("SELECT idjornada FROM Jornada WHERE jornada = @jornada AND idjornada <> @idjornada", dbC)
-        '    cmd.Parameters.AddWithValue("idjornada", idjornada)
-        '    cmd.Parameters.AddWithValue("jornada", jornada)
-        '    Dim rdr As SqlDataReader = cmd.ExecuteReader
-        '    If rdr.HasRows Then
-        '        rdr.Close()
-        '        err = "Error: no se actualizó, ya existe este nombre."
-        '    Else
-        '        rdr.Close()
-        '        cmd.CommandText = "UPDATE Jornada SET jornada = @jornada, inicio = @inicio, fin = @fin, color = @color, id_att = @id_att WHERE idjornada = @idjornada"
-        '        cmd.Parameters.AddWithValue("inicio", inicio)
-        '        cmd.Parameters.AddWithValue("fin", fin)
-        '        cmd.Parameters.AddWithValue("color", color)
-        '        cmd.Parameters.AddWithValue("id_att", id_att)
-        '        cmd.ExecuteNonQuery()
-        '        err = "Datos actualizados."
-        '    End If
-        '    rdr = Nothing : cmd.Dispose() : dbC.Close() : dbC.Dispose()
-        'End If
-        'Return err
+            Dim cmd As New SqlCommand("UPDATE Particulares SET tipo = @tipo, fecha = @fecha, observaciones = @observaciones ,cantidad = @cantidad,fecha_ued = @fecha_ued  WHERE idparticulares = @idparticulares", dbC)
+            cmd.Parameters.AddWithValue("tipo", tipo)
+            cmd.Parameters.AddWithValue("fecha", fecha)
+            cmd.Parameters.AddWithValue("idempleado", idempleado)
+            cmd.Parameters.AddWithValue("observaciones", observaciones)
+            cmd.Parameters.AddWithValue("cantidad", cantidad)
+            cmd.Parameters.AddWithValue("fecha_ued", fecha_ued)
+            cmd.Parameters.AddWithValue("idparticulares", idparticulares)
+            cmd.ExecuteNonQuery()
+            err = "Datos actualizados."
+
+            cmd.Dispose() : dbC.Close() : dbC.Dispose()
+        End If
+        Return err
     End Function
     Public Function eliminarParticulares(ByVal idparticulares As Integer) As String
         Dim dbC As New SqlConnection(StarTconnStrRH)
